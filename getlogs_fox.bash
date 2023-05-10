6329832ad25a28e8fb4683665f57bd203b6e3f3e
e-shrdlu <>
2023-05-10 11:19:34 -0500

    updated filepath in getlogs_fox.bash

--- getlogs_fox.bash
+++ getlogs_fox.bash
@@ -3,6 +3,6 @@
 # copies all log files from fox server to local machine
 
 src="ecc462@10.100.240.203:~/logs/"
-dst="/home/shrdlu/code/utsa-linux-server-usage-analysis/logs"
+dst="/home/shrdlu/code/Utsa-Server-Usage-Analysis/logs"
 
-rsync -a "$src" "$dst" 
+rsync -a "$src" "$dst"
