8cc11cd136918ce593e13b12c80fcd900f4cd183
e-shrdlu <>
2023-05-10 11:16:33 -0500

    added rolling average to total_load_over_time.py

--- analysis/total_load_over_time.py
+++ analysis/total_load_over_time.py
@@ -2,20 +2,24 @@ import process_data
 from process_data import server_datapoint, user_datapoint
 import matplotlib.pyplot as plt
 import datetime
+import numpy as np
 
+
+"""load logs"""
 log_path = "../logs"
 data = process_data.get_logs(log_path)
 
 x_vals=[]
 y_vals=[]
 
-timezone=datetime.timezone(datetime.timedelta(hours=-5))
 
 """options"""
-cutoff_before = datetime.datetime(2023,5,3,2, tzinfo=timezone)
+# timezone=datetime.timezone(datetime.timedelta(hours=-5)) # UTC-5
+# cutoff_before = datetime.datetime(2023,5,3,2, tzinfo=timezone)
+running_avg_window_size = int(15)
 
 
-# sum all datapoints within 5 minutes into one
+"""sum all datapoints within 5 minutes into one"""
 newdata = []
 while data:
     datapoint = data.pop()
@@ -29,15 +33,30 @@ while data:
     datapoint.server = "combined"
     newdata.append(datapoint)
 
+"""sort datapoints to be in chronological order"""
+newdata.sort(key=lambda x: x.time.timestamp())
+
+
+"""get x/y co-ordinates from data"""
 for datapoint in newdata:
-    if datapoint.time <= cutoff_before: # remove outlier?
-        continue
+    # if datapoint.time <= cutoff_before: # remove outlier?
+    #     continue
     x_vals.append(datapoint.time)
     y_vals.append(len(datapoint.users))
 
 
-l = plt.plot(x_vals, y_vals, "r-")
-plt.title("server usage over time")
+y_avg = []
+"""get running average"""
+for i in range(len(y_vals)):
+    left = max(i-running_avg_window_size, 0) # left end of range
+    right = min(i+running_avg_window_size, len(y_vals)) # right end of range
+    window = y_vals[left:right] # range to get avg from
+    y_avg.append(sum(window)/len(window)) # add average to list
+
+
+plt.plot(x_vals, y_vals, "ro")
+plt.plot(x_vals, y_avg, "b-")
+plt.title(f"server usage over time ({running_avg_window_size})")
 plt.xlabel("time")
 plt.ylabel("number of active logins")
 plt.show()
